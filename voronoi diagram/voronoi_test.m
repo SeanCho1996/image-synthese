98f17340cbeea129a32e15388ac822f9a922376a
seancho1996 <seanchozzx@outlook.com>
2019-10-06 14:55:06 +0200

    add vornoi_delauney

--- voronoi diagram/voronoi_test.m
+++ voronoi diagram/voronoi_test.m
@@ -1,8 +1,8 @@
-clc;
-clear;
+% clc;
+% clear;
 
 % define points
-points = 100*rand(5,2);
+% points = 100*rand(5,2);
 % figure
 % plot(points(:,1), points(:,2), 'bx');
 
@@ -21,3 +21,29 @@ subplot(122)
 plot(points(:,1), points(:,2), 'bx');
 hold on
 plot(hull(:,1),hull(:,2),'r');
+
+hull_inf = [];
+hull_sup = [];
+inf = 0;
+sup = 0;
+for i = 1: size(hull, 1)-1
+    k = (hull(i+1, 2) - hull(i, 2))/(hull(i+1, 1) - hull(i, 1));
+    b = hull(i+1, 2) - hull(i+1, 1)*k;
+    for j = 1:size(hull, 1)-1
+        if hull(j, 2) <= k*hull(j, 1) + b
+            inf = inf+1;
+        end
+        if hull(j, 2) >= k*hull(j, 1) + b
+            sup = sup+1;
+        end
+    end
+    if inf == size(hull, 1)-1
+        hull_inf = [hull_inf; hull(i,1:2); hull(i+1,1:2)];
+    elseif sup == size(hull, 1)-1
+        hull_sup = [hull_sup; hull(i,1:2); hull(i+1,1:2)];
+    end
+    inf = 0;
+    sup = 0;
+end
+plot(hull_inf(:,1),hull_inf(:,2), 'g');
+plot(hull_sup(:,1),hull_sup(:,2), 'y');
